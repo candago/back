5d921516065e4a0972c2c106611b16559092d686
carlostorres2305 <carlosadm15@hotmail.com>
2024-04-16 15:16:42 -0300

    função de buscas

--- src/controllers/db.ts
+++ src/controllers/db.ts
@@ -4,7 +4,7 @@ const pool = new Pool ({
     user : "postgres",
     host: "localhost",
     database : "bdapi",
-    password : "123",
+    password : "root",
     port : 5432
 });
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import aoi from "./aoi";
 import alteracao from "./alteracao";
 import apontamento from "./apontamento";
 import grade from "./grade";
+import { searchController } from "../controllers/searchController";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/aoi", aoi);
 router.use("/alteracao", alteracao);
 router.use("/apontamento", apontamento);
 router.use("/grade", grade);
+router.use("/search", searchController);
 
 export default router;
