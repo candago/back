b01aa06516034ac9cb9a815d9f42867ba872b758
jaqueslucas <lucassouza2347@hotmail.com>
2024-04-16 00:50:27 -0300

    habilita o cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
-// import cors from "cors";
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001 ;
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.listen(PORT,() => console.log(`Rodando na porta ${PORT}...`))
 
