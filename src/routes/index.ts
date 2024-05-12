d542465dcf86bd45ce824edb807bec0a58590f9e
Caique Moura <caique.fred@outlook.com>
2024-05-12 13:17:11 -0300

    update com branch testes. teste de rota dashboard

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import aoi from "./aoi";
 import alteracao from "./alteracao";
 import apontamento from "./apontamento";
 import grade from "./grade";
+import dashboard  from "./dashboard";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/aoi", aoi);
 router.use("/alteracao", alteracao);
 router.use("/apontamento", apontamento);
 router.use("/grade", grade);
+router.use("/dashboard", dashboard)
 
 export default router;
