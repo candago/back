c05e4dbec70e2b5a5ff17deb5a56b52bbb01d5fa
Caique Moura <caique.fred@outlook.com>
2024-05-13 22:23:06 -0300

    fix pool properties bd.ts

--- src/controllers/db.ts
+++ src/controllers/db.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 const pool = new Pool ({
     user : "postgres",
     host: "localhost",
-    database : "dbapi",
+    database : "bdapi",
     password : "123",
     port : 5432
 });
